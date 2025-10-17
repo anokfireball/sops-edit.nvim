f58bd34a857721fa49a9798ff4a7edd4abe93ec0
Fabian Koller <github@kthxbye.cyou>
2025-10-17 09:19:45 +0200

    feat: ini support, input validation, shell hardening

--- lua/sops_edit/utils.lua
+++ lua/sops_edit/utils.lua
@@ -1,36 +1,23 @@
 local M = {}
 
 function M.execute_command(cmd, input)
-	local exit_code
-	local stdout
-	local stderr
-
 	if input then
-		local result = vim.fn.system(cmd, input)
-		exit_code = vim.v.shell_error
-		stdout = result
-		if exit_code ~= 0 then
-			local err_result = vim.fn.system(cmd .. " 2>&1 >/dev/null", input)
-			stderr = err_result
+		local result = vim.fn.system(cmd .. " 2>&1", input)
+		local exit_code = vim.v.shell_error
+		if exit_code == 0 then
+			return true, result, ""
 		else
-			stderr = ""
+			return false, "", result
 		end
 	else
-		local result_list = vim.fn.systemlist(cmd)
-		exit_code = vim.v.shell_error
-		stdout = table.concat(result_list, "\n")
-		if exit_code ~= 0 then
-			local err_result_list = vim.fn.systemlist(cmd .. " 2>&1 >/dev/null")
-			stderr = table.concat(err_result_list, "\n")
+		local result_list = vim.fn.systemlist(cmd .. " 2>&1")
+		local exit_code = vim.v.shell_error
+		local output = table.concat(result_list, "\n")
+		if exit_code == 0 then
+			return true, output, ""
 		else
-			stderr = ""
+			return false, "", output
 		end
-	end
-
-	if exit_code == 0 then
-		return true, stdout, ""
-	else
-		return false, "", stderr
 	end
 end
 
